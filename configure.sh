61e0efeb8dd1892bfe98f6ad27126e936211a9bc
Alex <alexandr.koloskov@gmail.com>
2013-12-24 21:16:08 -0800

    Added ZFR for configure script

---
+++ configure.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+
