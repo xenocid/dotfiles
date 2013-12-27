3c1985ad98c03713c198808bd27ad28b766dc8f8
Alex <alexandr.koloskov@gmail.com>
2013-12-27 14:07:16 -0800

    Adding alias to always use macvim instead of system vim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ~="cd ~" # `cd` is probably faster to type though
 alias -- -="cd -"
+
+alias vim="/Applications/MacVim.app/Contents/MacOS/Vim"
