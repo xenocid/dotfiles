edd75f1c599b4a9fb92f6a5b0a0bfbed46f146c5
Alex <alexandr.koloskov@gmail.com>
2014-02-16 19:59:30 -0800

    Added LaunchRocket control panel

--- osx/configure.sh
+++ osx/configure.sh
@@ -3,3 +3,8 @@ cp -R -n ./osx/services/* ~/Library/Services/
 # Symlink PopClip settings
 unlink  ~/Library/Application\ Support/PopClip
 ln -s ~/Dropbox/Apps/PopClip ~/Library/Application\ Support/PopClip
+
+# Install LaunchRocket (https://github.com/jimbojsb/launchrocketo)
+# Homebrew services control panel
+brew tap jimbojsb/launchrocket
+brew cask install launchrocket
