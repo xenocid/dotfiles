c79004b64c41e387c2055cd997956962a62a3d08
Bob Gale <rgale@hpc-mac13.local>
2014-05-14 15:27:47 -0700

    add some ruby gems I use

--- ruby/configure.sh
+++ ruby/configure.sh
@@ -12,3 +12,14 @@ if ! rbenv versions | grep '2.1.0' 1>/dev/null
 then
 	rbenv install 2.1.0
 fi
+
+rbenv global 1.9.3-p484
+
+gem install lunchy
+lunchy install /usr/local/opt/mongodb/homebrew.mxcl.mongodb.plist
+lunchy install /usr/local/opt/redis/homebrew.mxcl.redis.plist
+
+gem install bundler
+
+heroku plugins:install git://github.com/ddollar/heroku-accounts.git
+
