1fa9142085fd04738974c9ac1af50b6cd88312b6
Alex Koloskov <alexandr.koloskov@gmail.com>
2014-10-19 16:08:45 -0700

    Updated versions of Ruby (MRI and JRuby)

--- ruby/configure.sh
+++ ruby/configure.sh
@@ -1,14 +1,14 @@
 if ! rbenv versions | grep 'jruby-1.7.9' 1>/dev/null
 then
-	rbenv install jruby-1.7.9
+	rbenv install jruby-1.7.16
 fi
 
 if ! rbenv versions | grep '1.9.3-p484' 1>/dev/null
 then
-	rbenv install 1.9.3-p484
+	rbenv install 1.9.3-p547
 fi
 
 if ! rbenv versions | grep '2.1.0' 1>/dev/null
 then
-	rbenv install 2.1.0
+	rbenv install 2.1.3
 fi
