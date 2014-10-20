7951b03125766b4b4b376e1f27756dc8c1c2d76f
Alex Koloskov <alexandr.koloskov@gmail.com>
2014-10-19 20:13:17 -0700

    Added install scripts for bunch of common apps via brew cask

--- osx/configure.sh
+++ osx/configure.sh
@@ -11,3 +11,15 @@ brew cask install launchrocket
 
 # Skip last login info output, makes shell to start a bit faster
 touch $HOME/.hushlogin
+
+brew cask install google-chrome
+brew cask install alfred
+brew cask install dropbox
+brew cask install textexpander
+brew cask install controlplane
+brew cask install virtualbox
+brew cask install calibre
+brew cask install omnifocus
+brew cask install onepassword
+
+brew cask alfred link
