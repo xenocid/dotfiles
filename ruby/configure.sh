6ff149d3b5bed968d0d3f3e0046cc317a094cb0c
Alex Koloskov <alexander.koloskov@gmail.com>
2014-10-20 14:51:35 -0700

    Removed JRuby installer and streamlined rbenv install commands

--- ruby/configure.sh
+++ ruby/configure.sh
@@ -1,16 +1,4 @@
-if ! rbenv versions | grep 'jruby-1.7.9' 1>/dev/null
-then
-	rbenv install -s jruby-1.7.16
-fi
-
-if ! rbenv versions | grep '1.9.3-p484' 1>/dev/null
-then
-	rbenv install -s 1.9.3-p547
-fi
-
-if ! rbenv versions | grep '2.1.0' 1>/dev/null
-then
-	rbenv install -s 2.1.3
-fi
+rbenv install -s 1.9.3-p547
+rbenv install -s 2.1.3
 
 rbenv shell 2.1.3
