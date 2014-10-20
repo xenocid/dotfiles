4bbe5e23c761864d10c1ca8c07e51a787213bd21
Alex Koloskov <alexandr.koloskov@gmail.com>
2014-10-19 20:12:53 -0700

    Skip ruby installation if it is already installed

--- ruby/configure.sh
+++ ruby/configure.sh
@@ -1,14 +1,14 @@
 if ! rbenv versions | grep 'jruby-1.7.9' 1>/dev/null
 then
-	rbenv -s install jruby-1.7.16
+	rbenv install -s jruby-1.7.16
 fi
 
 if ! rbenv versions | grep '1.9.3-p484' 1>/dev/null
 then
-	rbenv -s install 1.9.3-p547
+	rbenv install -s 1.9.3-p547
 fi
 
 if ! rbenv versions | grep '2.1.0' 1>/dev/null
 then
-	rbenv -s install 2.1.3
+	rbenv install -s 2.1.3
 fi
