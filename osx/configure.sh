87de2eb329ba00dc549eaf6f23a9ed95cd65e6f0
Alex Koloskov <alexander.koloskov@gmail.com>
2014-10-20 19:56:05 -0700

    Moved some osx installs to the cask file from configure script

--- osx/configure.sh
+++ osx/configure.sh
@@ -11,25 +11,3 @@ brew cask install launchrocket
 
 # Skip last login info output, makes shell to start a bit faster
 touch $HOME/.hushlogin
-
-brew cask install google-chrome
-brew cask install alfred
-brew cask install dropbox
-brew cask install textexpander
-brew cask install controlplane
-brew cask install virtualbox
-brew cask install calibre
-brew cask install omnifocus
-brew cask install onepassword
-brew cask install bettertouchtool
-brew cask install controlplane
-brew cask install crashplan
-brew cask install fitbit-connect
-brew cask install flowdock
-brew cask install google-hangouts
-brew cask install iterm2
-brew cask install karabiner
-brew cask install keyboard-maestro
-brew cask install moom
-
-brew cask alfred link
