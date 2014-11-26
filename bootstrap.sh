8df0b38b9825f2ee10a6d19fe544350bb9c9e717
Alex Koloskov <alexander.koloskov@gmail.com>
2014-11-26 13:09:22 -0800

    Moved common UI primitives into a shell library

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,23 +7,8 @@
 
 set -e
 
-info () {
-  printf "  [ \033[00;34m..\033[0m ] $1"
-}
-
-user () {
-  printf "\r  [ \033[0;33m?\033[0m ] $1 "
-}
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
+# Load UI routines
+. ui.sh
 
 add_usr_local_bin_to_path () {
 	if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; 
