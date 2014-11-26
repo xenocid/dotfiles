8457dc703d56361b699b8b9ea123e59471db1007
Alex Koloskov <alexander.koloskov@gmail.com>
2014-11-26 13:05:43 -0800

    Improve zsh detection. Format code. Improve feedback to the user. Switch to proper brew-cask installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,7 +26,8 @@ fail () {
 }
 
 add_usr_local_bin_to_path () {
-	if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
+	if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; 
+    then
 		info "Adding /usr/local/bin to PATH\n"
 		PATH="/usr/local/bin:$PATH"
 	fi
@@ -37,43 +38,60 @@ install_homebrew () {
 	then
 		info "Installing Homebrew...\n"
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        success "Done.\n"
 	else
-		info "Homebrew is already installed. Moving on.\n"
+		info "Homebrew is already installed.\n"
 	fi
-    brew tap phinze/cask
-    brew install brew-cask
+}
+
+install_brewcask () {
+    if (brew list | grep -w brew-cask >/dev/null); 
+    then
+        info "Brew Cask is already installed.\n"
+    else
+        info "Installing brew-cask...\n"
+        brew install brew-cask
+        success "Done.\n"
+    fi
 }
 
 install_git () {
-	if (brew list | grep -w git); then
-		info "Git is already installed. Moving on.\n"
+	if (brew list | grep -w git >/dev/null); 
+    then
+		info "Git is already installed.\n"
 	else
 		info "Installing git...\n"
 		brew install git
+        success "Done.\n"
 	fi
 }
 
 install_zsh () {
-	if (brew list | grep -w zsh); then 
-		info "Zsh is already installed. Moving on.\n"
+	if (brew list | grep -w zsh >/dev/null); 
+    then 
+		info "Zsh is already installed.\n"
 	else
 		info "Installing zsh...\n"
 		brew install zsh
+        success "Done.\n"
 	fi
 }
 
 make_zsh_default () {
-	if [ "$ZSH_NAME" ]; 
-	then
-		info "Already running zsh. Moving on.\n"
+    if [ "$SHELL" == "zsh" ];
+    then 
+		info "Already running zsh.\n"
 	else
 		info "Switching to zsh...\n"
 		chsh -s $(which zsh)
+        success "Done.\n"
 	fi
 }
 
 add_usr_local_bin_to_path
 install_homebrew
+install_brewcask
 install_git
 install_zsh
 make_zsh_default
+success "Bootstrapping is complete\n"
