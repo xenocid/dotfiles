7a74570b8319d59adaf7907e6cf68a435ca89f82
Alex Koloskov <alexander.koloskov@gmail.com>
2014-11-26 16:22:15 -0800

    Add extra highlighters

--- zsh/zsh-highlighters.zsh
+++ zsh/zsh-highlighters.zsh
@@ -1,2 +1,2 @@
 source ~/Config/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main root)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main root brackets cursor line pattern)
